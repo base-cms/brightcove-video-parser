a82283a8c05248692b38fdccd16470abe5f6b850
Josh Worden <solocommand@gmail.com>
2019-07-09 14:23:04 -0500

    Update slack notifications to use environment vars

--- scripts/deploy-k8s.sh
+++ scripts/deploy-k8s.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
-
+./scripts/deploy-notify-start.sh
 IMAGE=basecms/brightcove-video-parser:$1
 yarn global add @endeavorb2b/rancher2cli
 r2 dl basecms-service brightcove-video-parser $IMAGE --namespace=default
+./scripts/deploy-notify.sh
--- scripts/deploy-notify-start.sh
+++ scripts/deploy-notify-start.sh
@@ -4,8 +4,8 @@ ENVIRONMENT="production"
 
 payload="{
   \"attachments\": [{
-    \"color\": \"good\",
-    \"text\": \"Deployment of \`$TRAVIS_REPO_SLUG\` @ \`$TRAVIS_TAG\` to \`$ENVIRONMENT\` has finished.\"
+    \"color\": \"#1d9bd1\",
+    \"text\": \"Deployment of \`$TRAVIS_REPO_SLUG\` @ \`$TRAVIS_TAG\` to \`$ENVIRONMENT\` has started.\"
   }]
 }"
 curl -f -X POST --data "$payload" -H 'Content-type: application/json' $SLACK_WEBHOOK_URL
